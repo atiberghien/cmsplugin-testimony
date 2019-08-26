8d70096677586e20982a561260c1504f7ba27a75
Alban Tiberghien <alban.tiberghien@gmail.com>
2019-08-26 13:14:01 +0200

    Retro compat

--- testimony/migrations/0001_initial.py
+++ testimony/migrations/0001_initial.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-
+import sys
 
 class Migration(migrations.Migration):
 
@@ -10,14 +10,22 @@ class Migration(migrations.Migration):
         ('cms', '0016_auto_20160608_1535'),
     ]
 
+    if sys.version_info[0] < 3:
+        fields=[
+            ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ('witness', models.CharField(max_length=500)),
+            ('story', models.TextField()),
+        ]
+    else:
+        fields=[
+            ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True, on_delete=models.CASCADE)),
+            ('witness', models.CharField(max_length=500)),
+            ('story', models.TextField()),
+        ]
     operations = [
         migrations.CreateModel(
             name='Testimony',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True, on_delete=on_delete=models.CASCADE)),
-                ('witness', models.CharField(max_length=500)),
-                ('story', models.TextField()),
-            ],
+            fields=fields
         ),
         migrations.CreateModel(
             name='TestimonyPluginModel',
